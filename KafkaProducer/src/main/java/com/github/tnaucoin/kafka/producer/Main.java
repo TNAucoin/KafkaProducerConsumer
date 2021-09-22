c6dff6596d9f3faf1d3a7c55a8f839edd35289df
TNAucoin <travisdevaucoin@gmail.com>
2021-09-22 08:51:05 -0500

    Finished Basic Producer Events
    producer now generates and sends events to kafka

--- KafkaProducer/src/main/java/com/github/tnaucoin/kafka/producer/Main.java
+++ KafkaProducer/src/main/java/com/github/tnaucoin/kafka/producer/Main.java
@@ -1,10 +1,14 @@
 package com.github.tnaucoin.kafka.producer;
 
+import com.github.tnaucoin.kafka.producer.model.Event;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
 import java.util.Properties;
+
+import static org.apache.kafka.common.utils.Utils.sleep;
 
 @Slf4j
 public class Main {
@@ -17,5 +21,27 @@ public class Main {
         props.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
 
         Producer<String,String> producer = new KafkaProducer<String, String>(props);
+
+        for(int i = 0; i <= 10; i++){
+            log.info("Generating Event #" + i);
+            Event event = eventGenerator.generateEvent();
+            String key = extractKey(event);
+            String value = extractValue(event);
+
+            ProducerRecord<String,String> producerRecord = new ProducerRecord<>("user-tracking",key,value);
+            log.info("Sending Event to Kafka: " + key + ":" + value);
+            producer.send(producerRecord);
+            sleep(1000);
+        }
+
+        producer.close();
+    }
+
+    private static String extractKey(Event event) {
+        return event.getUser().getUserId().toString();
+    }
+
+    private static String extractValue(Event event) {
+        return String.format("%s, %s, %s",event.getProduct().getProductType(), event.getProduct().getColor(), event.getProduct().getDesignType());
     }
 }
